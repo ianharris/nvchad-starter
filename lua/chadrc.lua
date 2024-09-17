b40149109a665e73d4eebc8369aa363fb4a6bcea
Ian Harris <ianjamesharris@gmail.com>
2024-09-17 22:13:41 +0100

    update statusline configuration

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,19 +1,22 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
   theme = "chadracula-evondev",
-  statusline = {
-    theme = "vscode_colored",
-  },
   -- hl_override = {
   -- 	Comment = { italic = true },
   -- 	["@comment"] = { italic = true },
   -- },
+}
+
+M.ui = {
+  statusline = {
+    theme = "vscode_colored",
+  },
 }
 
 return M
